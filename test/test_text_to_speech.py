fbe9343430842ad0efb8e2efc54d1ab43a576800
dde227 <84485283+dde227@users.noreply.github.com>
2021-06-24 22:34:04 -0400

    update to pytest

--- test/test_text_to_speech.py
+++ test/test_text_to_speech.py
@@ -3,6 +3,13 @@ import os
 
 def test_convert():
 
+    mp3_file_path = os.path.join(os.path.dirname(__file__),"..","mockaudio.mp3")
+
+    if os.path.isfile(mp3_file_path):
+        os.remove(mp3_file_path)
+    
+    assert os.path.isfile(mp3_file_path) == False
+    
     file_path = os.path.join(os.path.dirname(__file__),"mock_data","sample_text.pdf")
     output_name = "mockaudio"
     language = "fr-FR"
@@ -11,3 +18,5 @@ def test_convert():
 
 
     convert(pdf_file_path=file_path, output_name = output_name, language = language, gender = gender, voice_type = voice_type)
+
+    assert os.path.isfile(mp3_file_path) == True
